c3b8f15aa96e38f0868ec2e86518f5bcc4864386
Nitzan Frock <nitzanf@novatec.com>
2018-02-01 16:03:20 -0500

    moved cancel and continue buttons on orderSummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -22,8 +22,8 @@ const orderSummary = (props) => {
             </ul>
             <p><strong>Order Price: ${props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType={"Danger"} clicked={props.purchaseCancelled} >CANCEL</Button>
             <Button btnType={"Success"} clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType={"Danger"} clicked={props.purchaseCancelled} >CANCEL</Button>
         </Auxiliary>
     );
 };
